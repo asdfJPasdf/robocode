933a5a0eb1222e7bdd13c1af3e9fe4ea8e4ed458
jonas <jonas.pfammatter@lernende.bfo-vs.ch>
2022-12-05 11:39:27 +0100

    add robot

---
+++ robocode/roobo/src/test/Cybertank.java
@@ -0,0 +1,45 @@
+package test;
+import robocode.ScannedRobotEvent;
+
+import java.awt.Color;
+
+import robocode.AdvancedRobot;
+
+public class Cybertank extends AdvancedRobot {
+
+	private Cybertank_radar radar;
+	private Cybertank_cannon cannon;
+	private Cybertank_tank tank;
+	private int turn;
+
+	public void run() {
+		setColors(Color.BLACK, Color.BLACK, Color.BLACK);
+		setAdjustRadarForGunTurn(true); // our radar, gun and tank
+		setAdjustGunForRobotTurn(true); // are independently controlled
+		turn = 0;
+		radar = new Cybertank_radar(this);
+		cannon = new Cybertank_cannon(this);
+		tank = new Cybertank_tank(this);
+		while (true) {
+			turn++;
+			tank.move();
+			radar.look();
+			cannon.fire();
+			execute();
+		}
+	}
+
+	/**
+	 * onScannedRobot: keep scan info on the last robot seen
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		cannon.enemySeen(turn, e);
+	}
+
+	// Our components may need information from us.
+	// Here is the current turn
+	public int getTurn() {
+		return turn;
+	}
+
+}
